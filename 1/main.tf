cab410c737d1a801518c6bcd957838dd25a78c70
pandeysp1 <119328533+pandeysp1@users.noreply.github.com>
2024-03-13 14:16:47 +0530

    Update main.tf

--- 1/main.tf
+++ 1/main.tf
@@ -2,8 +2,8 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 10
   engine               = "mysql"
   instance_class       = "db.t3.micro"
-  username             = "foo"
-  password             = "foobarbaz"
+  username             = "pan"
+  password             = "pan12345"
   skip_final_snapshot = true // required to destroy
 }
 
